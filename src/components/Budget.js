3bebf1e3566941b795deee0dbe20b87e6e33ac6d
Rabidalton <rabiakalimat02@gmail.com>
2023-08-25 12:36:30 -0400

    budget component edited.

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,7 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const [setBudget] = useState('');
+
     return (
         <div className='alert alert-secondary'>
             <span>
@@ -9,14 +12,20 @@ const Budget = () => {
                 <input
                     required='required'
                     type='number'
+                    step='10'
+                    min='0'
+                    max='20000'
                     pattern = "[0-9]*"
                     id='budget'
-                    value={budget}
+                    defaultValue={budget}
                     style={{ marginLeft: '2rem' , size: 10}}
+                    onChange={(event) => setBudget((v) => 
+                        (event.target.validity.valid ? event.target.value : v))}
                 >
                 </input>
             </span>
         </div>
+        
     );
 };
 export default Budget;
