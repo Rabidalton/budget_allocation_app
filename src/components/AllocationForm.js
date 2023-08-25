bc2f1c71d685302ca401d79eaec98522691fe213
Rabidalton <rabiakalimat02@gmail.com>
2023-08-25 13:35:42 -0400

    new commits

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -54,7 +54,7 @@ const AllocationForm = (props) => {
                     <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
                 <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
                   </div>
-                  <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
+                  <select className="custom-select" style={{marginRight: '2rem'}} id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
